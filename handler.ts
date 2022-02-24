015cb384932624705f953c790feec5964185e715
atlasrw <atlasrw@icloud.com>
2022-02-24 04:29:56 +0000

    Lambda Functions

--- handler.ts
+++ handler.ts
@@ -1,12 +1,22 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyHandler } from 'aws-lambda'
+import urlExist from 'url-exists-nodejs'
+import Gateway from './gateway'
 
-export const hello: APIGatewayProxyHandler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v2.0! Your function executed successfully!",
-      context,
-      event,
-    }),
-  };
-};
+export const hello: APIGatewayProxyHandler = async () =>
+  Gateway.Result(
+    200,
+    'Hello World!'
+  )
+
+export const echo: APIGatewayProxyHandler = async (event: APIGatewayEvent) =>
+  Gateway.Result(
+    200,
+    event.body
+  )
+
+export const checkUrl: APIGatewayProxyHandler = async (event: APIGatewayEvent) =>
+  Gateway.Result(
+    await urlExist(event.body)
+      ? 200
+      : 404
+  )
